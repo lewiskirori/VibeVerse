bc06dbf954dfc37943965e71aa60ee2f5d325e0b
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2025-11-17 10:29:49 +0300

    Fix reset configuration

--- config/auth.php
+++ config/auth.php
@@ -89,12 +89,12 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
+    'passwords'=> [
+        'users'=> [
+            'provider'=> 'users',
+            'table'=> 'password_reset_tokens',
+            'throttle'=> 60,
+            'expire'=> 60,
         ],
     ],
 
