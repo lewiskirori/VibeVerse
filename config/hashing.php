a61ce208b4e386a2d40a3679dc846c0b7be4a43b
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2025-11-21 09:17:43 +0300

    Fix Argon hashing configuration

--- config/hashing.php
+++ config/hashing.php
@@ -40,13 +40,12 @@ return [
     | Here you may specify the configuration options that should be used when
     | passwords are hashed using the Argon algorithm. These will allow you
     | to control the amount of time it takes to hash the given password.
-    |
     */
 
-    'argon' => [
-        'memory' => 65536,
-        'threads' => 1,
-        'time' => 4,
+    'argon'=> [
+       'memory'=> 65536,
+       'threads'=> 1,
+       'time'=> 4,
     ],
 
 ];
