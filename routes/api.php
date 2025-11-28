54b9fc821543cf9f13a852d665edb85b989be2b1
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2025-11-28 07:28:39 +0300

    Format routes for consistency

--- routes/api.php
+++ routes/api.php
@@ -11,19 +11,17 @@ use App\Http\Controllers\LikeController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|
 */
 
 // Public routes
-Route::post('/signup', [AuthController::class, 'register']);
-Route::post('/signin', [AuthController::class, 'login']);
+Route::post('/signup',[AuthController::class, 'register']);
+Route::post('/signin',[AuthController::class, 'login']);
 
 // Protected routes
-Route::group(['middleware' => ['auth:sanctum']], function() {
+Route::group(['middleware'=> ['auth:sanctum']], function() {
     
     // User
     Route::post('/enjoyer', [AuthController::class, 'user']);
