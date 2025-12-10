d3e9e60cf1ec5c625f6fe57ddfa8a40a2efbde3f
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2025-12-10 15:42:11 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,6 +17,7 @@ define('LARAVEL_START', microtime(true));
 */
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    
     require $maintenance;
 }
 
@@ -28,7 +29,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
 | into the script here so we don't need to manually load our classes.
-|
 */
 
 require __DIR__.'/../vendor/autoload.php';
@@ -41,15 +41,15 @@ require __DIR__.'/../vendor/autoload.php';
 | Once we have the application, we can handle the incoming request using
 | the application's HTTP kernel. Then, we will send the response back
 | to this client's browser, allowing them to enjoy our application.
-|
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
+$kernel = $app -> make(Kernel :: class);
+
+$response = $kernel -> handle(
+    $request = Request :: capture()
 )->send();
 
 $kernel->terminate($request, $response);
