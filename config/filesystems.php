329d99c7e5a474aba1bc8a95e4e2a6c712f3c3b1
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2026-03-22 10:12:51 +0300

    Update configuration for profiles

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,7 @@ return [
         'profiles' => [
             'driver' => 'local',
             'root' => storage_path('app/public/profiles'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/',
             'visibility' => 'public',
         ],
 
