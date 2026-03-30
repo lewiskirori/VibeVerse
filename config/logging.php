2c61b4c34169b00c25a0bc2c8b95ba97d04e789a
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2026-03-30 08:07:16 +0300

    Change driver config

--- config/logging.php
+++ config/logging.php
@@ -95,7 +95,7 @@ return [
         ],
 
         'stderr' => [
-            'driver' => 'monolog',
+            'driver' => 'stderr',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
