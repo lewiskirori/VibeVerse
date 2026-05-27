e62f351b46dff617c3eb0f189abbd6bab05a1515
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2026-05-27 17:19:03 +0300

    Fix formatting CApp

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -9,7 +9,7 @@ trait CreatesApplication
 {
     /**
      * Creates the application.
-     */
+     **/
     public function createApplication(): Application
     {
         $app = require __DIR__.'/../bootstrap/app.php';
